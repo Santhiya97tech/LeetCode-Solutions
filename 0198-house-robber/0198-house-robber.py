0aed0c4bdf472dcce3c795aae65a0870ce069f17
santhiya <santhiyapanneer2812@gmail.com>
2026-04-08 17:07:27 +0530

    Time: 0 ms (100%), Space: 12.3 MB (92.94%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def rob(self, nums):
+        prev1=0
+        prev2=0
+        for num in nums:
+            temp=prev1
+            prev1=max(prev1,prev2+num)
+            prev2=temp
+        return prev1    
